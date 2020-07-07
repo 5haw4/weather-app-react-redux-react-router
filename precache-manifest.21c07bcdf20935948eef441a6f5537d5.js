93f84415118cd11b1d61e815627e45624ea855b5
5haw4 <52867065+5haw4@users.noreply.github.com>
2020-07-07 12:56:50 +0300

    Updates

---
+++ precache-manifest.21c07bcdf20935948eef441a6f5537d5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8096eafcb645aff12b8db6ebf1e5298",
+    "url": "/shawn-weather-05-07-2020/index.html"
+  },
+  {
+    "revision": "11abe34abaa3327cee85",
+    "url": "/shawn-weather-05-07-2020/static/css/2.c4a494fa.chunk.css"
+  },
+  {
+    "revision": "ed394c8fb3e0230a6275",
+    "url": "/shawn-weather-05-07-2020/static/css/main.90f845ac.chunk.css"
+  },
+  {
+    "revision": "11abe34abaa3327cee85",
+    "url": "/shawn-weather-05-07-2020/static/js/2.daa46f27.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/shawn-weather-05-07-2020/static/js/2.daa46f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed394c8fb3e0230a6275",
+    "url": "/shawn-weather-05-07-2020/static/js/main.86494c18.chunk.js"
+  },
+  {
+    "revision": "ff63286183d7f18a2a71",
+    "url": "/shawn-weather-05-07-2020/static/js/runtime-main.dee0830c.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/shawn-weather-05-07-2020/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
